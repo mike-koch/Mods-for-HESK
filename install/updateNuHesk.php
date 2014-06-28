a21f59f91eb492993eb96a5ef6b2f87df9e33ae3
Mike Koch <mkoch227@gmail.com>
2014-06-28 15:57:14 -0400

    Updated installation script for updated table.

--- install/updateNuHesk.php
+++ install/updateNuHesk.php
@@ -4,51 +4,57 @@ define('HESK_PATH','../');
 require(HESK_PATH . 'install/install_functions.inc.php');
 require(HESK_PATH . 'hesk_settings.inc.php');
 
+$showFinished = 'none';
+$showInstructions = 'block';
 if ($_GET['update'] == 1)
 {
     hesk_dbConnect();
     $somethingRS = hesk_dbQuery("SHOW TABLES LIKE '".$hesk_settings['db_pfix']."statuses'");
     $shouldBuildTable = ($somethingRS->num_rows == 0);
-    $showInstructions = 'block';
-    $showFinished = 'none';
+    $showInstructions = 'none';
+    $showFinished = 'block';
 
     if ($shouldBuildTable)
     {
         $showInstructions = 'none';
         $showFinished = 'block';
 
-        hesk_dbQuery("CREATE TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (
-                        `ID` INT NOT NULL AUTO_INCREMENT,
-                        `ShortNameContentKey` TEXT NOT NULL,
-                        `TicketViewContentKey` TEXT NOT NULL,
-                        `TextColor` TEXT NOT NULL,
-                        `IsClosed` BIT NOT NULL DEFAULT 0,
-                        `IsClosedByClient` BIT NOT NULL DEFAULT 0,
-                        `IsStaffClosedOption` BIT NOT NULL DEFAULT 0,
-                        `IsStaffReopenedStatus` BIT NOT NULL DEFAULT 0,
-                        `IsDefaultStaffReplyStatus` BIT NOT NULL DEFAULT 0,
-                        `LockedTicketStatus` BIT NOT NULL DEFAULT 0,
-                        PRIMARY KEY (`ID`))");
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (0, 'open', 'open', '#FF0000', 0, 0, 0, 0, 0, 0);");
-        hesk_dbQuery("UPDATE `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` SET ID = 0 WHERE ID = 1;");
+        //-- Need to do this since we are no longer restricted on IDs and we want an INT for proper INNER JOINs
+        hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."tickets` CHANGE COLUMN `status` `status` INT NOT NULL DEFAULT '0'");
 
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (1, 'wait_reply', 'wait_staff_reply', '#FF9933', 0, 0, 0, 1, 0, 0);");
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (2, 'replied', 'wait_cust_reply', '#0000FF', 0, 0, 0, 0, 1, 0);");
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (3, 'closed', 'closed', '#008000', 1, 1, 1, 0, 0, 1);");
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (4, 'in_progress', 'in_progress', '#000000', 0, 0, 0, 0, 0, 0);");
-        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsClosed, IsClosedByClient,
-                    IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
-                    VALUES (5, 'on_hold', 'on_hold', '#000000', 0, 0, 0, 0, 0, 0);");
+        hesk_dbQuery("CREATE TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (
+                      `ID` INT NOT NULL,
+                      `ShortNameContentKey` TEXT NOT NULL,
+                      `TicketViewContentKey` TEXT NOT NULL,
+                      `TextColor` TEXT NOT NULL,
+                      `IsNewTicketStatus` BIT NOT NULL DEFAULT 0,
+                      `IsClosed` BIT NOT NULL DEFAULT 0,
+                      `IsClosedByClient` BIT NOT NULL DEFAULT 0,
+                      `IsCustomerReplyStatus` BIT NOT NULL DEFAULT 0,
+                      `IsStaffClosedOption` BIT NOT NULL DEFAULT 0,
+                      `IsStaffReopenedStatus` BIT NOT NULL DEFAULT 0,
+                      `IsDefaultStaffReplyStatus` BIT NOT NULL DEFAULT 0,
+                      `LockedTicketStatus` BIT NOT NULL DEFAULT 0,
+                        PRIMARY KEY (`ID`))");
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (0, 'open', 'open', '#FF0000', 1, 0, 0, 0, 0, 0, 0, 0);");
+
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (1, 'wait_reply', 'wait_staff_reply', '#FF9933', 0, 0, 0, 1, 0, 1, 0, 0);");
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (2, 'replied', 'wait_cust_reply', '#0000FF', 0, 0, 0, 0, 0, 0, 1, 0);");
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (3, 'closed', 'closed', '#008000', 0, 1, 1, 0, 1, 0, 0, 1);");
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (4, 'in_progress', 'in_progress', '#000000', 0, 0, 0, 0, 0, 0, 0, 0);");
+        hesk_dbQuery("INSERT INTO `".hesk_dbEscape($hesk_settings['db_pfix'])."statuses` (ID, ShortNameContentKey, TicketViewContentKey, TextColor, IsNewTicketStatus, IsClosed, IsClosedByClient, IsCustomerReplyStatus,
+		IsStaffClosedOption, IsStaffReopenedStatus, IsDefaultStaffReplyStatus, LockedTicketStatus)
+	VALUES (5, 'on_hold', 'on_hold', '#000000', 0, 0, 0, 0, 0, 0, 0, 0);");
     }
 }
 
