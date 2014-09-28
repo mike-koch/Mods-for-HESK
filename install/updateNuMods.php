df847c7e18899b0296976841750fc03903bfd53a
Mike Koch <mkoch227@gmail.com>
2014-09-28 10:24:36 -0400

    Fixed update script for those who attempted the 1.4.0 installation

--- install/updateNuMods.php
+++ install/updateNuMods.php
@@ -73,6 +73,7 @@ if ($_GET['update'] == 1)
     <body>
         <div style="display: <?php echo $showInstructions; ?>">
         <h1>Update NuMods from v1.2.4 - v1.3.0 to v1.4.1</h1>
+		<p>If you attempted the v1.4.0 installation and it failed, use <a href="updateTo1-4-1.php?ar=true">this update link</a>. Do not use the link below!</p>
         <p><a href="updateTo1-4-1.php">Update here</a>. <b>Do not use the installation below!</b></p>
         <h1>Install NuMods v1.4.1 <b>for the first time</b></h1>
         <h4><i>If you have not yet installed/updated HESK, please do so first before continuing; otherwise installation will <b>fail</b>!</i></h4>
--- install/updateTo1-4-1.php
+++ install/updateTo1-4-1.php
@@ -4,11 +4,14 @@ define('HESK_PATH','../');
 require(HESK_PATH . 'install/install_functions.inc.php');
 require(HESK_PATH . 'hesk_settings.inc.php');
 hesk_dbConnect();
-hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."users` ADD COLUMN `autorefresh` BIGINT NOT NULL DEFAULT 0 AFTER `replies`;");
-hesk_dbQuery("CREATE TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."denied_ips` (
-  `ID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `RangeStart` VARCHAR(100) NOT NULL,
-  `RangeEnd` VARCHAR(100) NOT NULL)");
+if (!isset($_GET['ar'])) {
+	hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."users` ADD COLUMN `autorefresh` BIGINT NOT NULL DEFAULT 0 AFTER `replies`;");
+
+	hesk_dbQuery("CREATE TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."denied_ips` (
+	  `ID` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	  `RangeStart` VARCHAR(100) NOT NULL,
+	  `RangeEnd` VARCHAR(100) NOT NULL)");
+}
 hesk_dbQuery("CREATE TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."denied_emails` (ID INT NOT NULL PRIMARY KEY AUTO_INCREMENT, Email VARCHAR(100) NOT NULL);");
 hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."tickets` ADD COLUMN `parent` MEDIUMINT(8) NULL AFTER `custom20`;");
 ?>
