c4a50d10f72673355d92251083f4fcfe05ebeea3
Mike Koch <mkoch227@gmail.com>
2014-10-24 23:55:29 -0400

    Missed some content

--- language/en/text.php
+++ language/en/text.php
@@ -32,6 +32,7 @@ $hesklang['user_activated'] = 'User has been activated';
 $hesklang['user_deactivated'] = 'User has been deactivated';
 $hesklang['active_user'] = 'Is active user';
 $hesklang['self_deactivation'] = 'You cannot deactivate yourself!';
+$hesklang['inactive_user'] = 'Your account is currently inactive. Contact an administrator for more information.';
 
 // ADDED OR MODIFIED IN NuMods 1.4.0
 $hesklang['numods'] = 'NuMods';
