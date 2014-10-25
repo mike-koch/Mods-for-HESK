7c40c25cff780054fbbf3ddb48b6c3a5e09e338f
Mike Koch <mkoch227@gmail.com>
2014-10-25 00:14:15 -0400

    #62 The send email property should be set to false by default

--- install/updateTo1-5-0.php
+++ install/updateTo1-5-0.php
@@ -6,6 +6,7 @@ require(HESK_PATH . 'hesk_settings.inc.php');
 hesk_dbConnect();
 hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."users` ADD COLUMN `active` ENUM('0', '1') NOT NULL DEFAULT '1'");
 hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."users` ADD COLUMN `can_manage_settings` ENUM('0', '1') NOT NULL DEFAULT '1'");
+hesk_dbQuery("ALTER TABLE `".hesk_dbEscape($hesk_settings['db_pfix'])."users` ADD COLUMN `default_notify_customer_email` ENUM ('0', '1') NOT NULL DEFAULT '0'");
 ?>
 
 <h1>Update complete!</h1>
