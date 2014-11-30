d26ed0bac199c67a987f8422eda094e1c1fef288
Mike Koch <mkoch227@gmail.com>
2014-11-29 22:10:57 -0500

    Run the query before hiding specific fields in demo mode

--- admin/admin_settings.php
+++ admin/admin_settings.php
@@ -77,6 +77,10 @@ require_once(HESK_PATH . 'inc/headerAdmin.inc.php');
 // Print main manage users page
 require_once(HESK_PATH . 'inc/show_admin_nav.inc.php');
 
+// Get the current version of Mods for Hesk
+$modsForHeskVersionRS = hesk_dbQuery("SELECT `Value` FROM `".hesk_dbEscape($hesk_settings['db_pfix'])."settings` WHERE `Key` = 'modsForHeskVersion'");
+$modsForHeskVersion = hesk_dbFetchAssoc($modsForHeskVersionRS)['Value'];
+
 // Demo mode? Hide values of sensitive settings
 if ( defined('HESK_DEMO') )
 {
@@ -95,9 +99,6 @@ if ( defined('HESK_DEMO') )
 	$hesk_settings['recaptcha_private_key']	= $hesklang['hdemo'];
 }
 
-// Get the current version of Mods for Hesk
-$modsForHeskVersionRS = hesk_dbQuery("SELECT `Value` FROM `".hesk_dbEscape($hesk_settings['db_pfix'])."settings` WHERE `Key` = 'modsForHeskVersion'");
-$modsForHeskVersion = hesk_dbFetchAssoc($modsForHeskVersionRS)['Value'];
 ?>
 
 <div class="row" style="margin-top: 20px">
