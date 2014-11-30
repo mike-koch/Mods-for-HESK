f339dbc2ba076dd86755f52aaa22b344b587213b
Mike Koch <mkoch227@gmail.com>
2014-11-29 21:51:07 -0500

    Wrap each link an <li> tag

--- install/updateModsForHesk.php
+++ install/updateModsForHesk.php
@@ -12,11 +12,11 @@ require(HESK_PATH . 'hesk_settings.inc.php');
         <h1>Mods for HESK 1.6.0 Install / Upgrade</h1>
         <h3>Select your current Mods for HESK version number to upgrade.</h3>
         <ul style="list-style-type: none">
-            <a href="updateTo1-6-0.php">v1.5.0</a>
-            <a href="updateTo1-5-0.php">v1.4.1</a>
-            <a href="updateTo1-4-1.php?ar=true">v1.4.0</a>
-            <a href="updateTo1-4-1.php">v1.2.4 or v1.3.0</a>
-            <a href="freshInstall.php">No previous version (make sure you have installed/updated HESK first; otherwise installation will <b>fail</b>!)</a>
+            <li><a href="updateTo1-6-0.php">v1.5.0</a></li>
+            <li><a href="updateTo1-5-0.php">v1.4.1</a></li>
+            <li><a href="updateTo1-4-1.php?ar=true">v1.4.0</a></li>
+            <li><a href="updateTo1-4-1.php">v1.2.4 or v1.3.0</a></li>
+            <li><a href="freshInstall.php">No previous version</a> (make sure you have installed/updated HESK first; otherwise installation will <b>fail</b>!)</li>
         </ul>
         <p>Please verify the database information below.  Additionally, ensure that the database user has CREATE and ALTER permissions.</p>
         <p><b>Database Host: </b> <?php echo $hesk_settings['db_host']; ?></p>
