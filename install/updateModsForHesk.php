33ec8ac79aba3540888a1153e7b5ed7f88feee49
Mike Koch <mkoch227@gmail.com>
2014-11-29 21:28:51 -0500

    Hopefully making the installation script easier to run

--- install/updateModsForHesk.php
+++ install/updateModsForHesk.php
@@ -9,23 +9,22 @@ require(HESK_PATH . 'hesk_settings.inc.php');
         <title>Mods For HESK 1.6.0 Install / Upgrade</title>
     </head>
     <body>
-        <h1>Update Mods for HESK from v1.5.0 to v1.6.0</h1>
-        <p><a href="updateTo1-6-0.php">Update here</a>. <b>Do not use the installation below!</b> </p>
-        <h1>Update Mods For HESK from v1.4.1 to v1.5.0</h1>
-        <p><a href="updateTo1-5-0.php">Update here</a>. <b>Do not use the installation below!</b> </p>
-        <h1>Update Mods For HESK from v1.2.4 - v1.3.0 to v1.5.0</h1>
-		<p>If you attempted the v1.4.0 installation and it failed, use <a href="updateTo1-4-1.php?ar=true">this update link</a>. Do not use the link below!</p>
-        <p><a href="updateTo1-4-1.php">Update here</a>. <b>Do not use the installation below!</b></p>
-        <h1>Install Mods For HESK v1.5.0 <b>for the first time</b></h1>
-        <h4><i>If you have not yet installed/updated HESK, please do so first before continuing; otherwise installation will <b>fail</b>!</i></h4>
-        <br/>
+        <h1>Mods for HESK 1.6.0 Install / Upgrade</h1>
+        <h3>Select your current Mods for HESK version number to upgrade.</h3>
+        <ul style="list-style-type: none">
+            <a href="updateTo1-6-0.php">v1.5.0</a>
+            <a href="updateTo1-5-0.php">v1.4.1</a>
+            <a href="updateTo1-4-1.php?ar=true">v1.4.0</a>
+            <a href="updateTo1-4-1.php">v1.2.4 or v1.3.0</a>
+            <a href="freshInstall.php">No previous version (make sure you have installed/updated HESK first; otherwise installation will <b>fail</b>!)</a>
+        </ul>
         <p>Please verify the database information below.  Additionally, ensure that the database user has CREATE and ALTER permissions.</p>
         <p><b>Database Host: </b> <?php echo $hesk_settings['db_host']; ?></p>
         <p><b>Database Name: </b><?php echo $hesk_settings['db_name']; ?></p>
         <p><b>Database User: </b><?php echo $hesk_settings['db_user']; ?></p>
         <p><b>Database Password: </b><?php echo $hesk_settings['db_pass']; ?></p>
         <p><b>Database Prefix: </b><?php echo $hesk_settings['db_pfix']; ?></p>
-        <a href="freshInstall.php">Proceed with installation</a>
+        
         <p>By proceeding, you agree to the terms of the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License.</a></p>
     </body>
 </html>
