cd879f9898f168caae7cf94ccbd4c031b61eeaa5
Mike Koch <mkoch227@gmail.com>
2015-01-12 15:27:31 -0500

    #92 Update show_admin_nav.inc.php

--- inc/show_admin_nav.inc.php
+++ inc/show_admin_nav.inc.php
@@ -1,7 +1,7 @@
 <?php
 /*******************************************************************************
 *  Title: Help Desk Software HESK
-*  Version: 2.5.5 from 5th August 2014
+*  Version: 2.6.0 beta 1 from 30th December 2014
 *  Author: Klemen Stirn
 *  Website: http://www.hesk.com
 ********************************************************************************
@@ -52,14 +52,38 @@ $num_mail = hesk_checkNewMail();
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
           <li><a href="admin_main.php"><i class="fa fa-home" <?php echo $iconDisplay; ?>></i>&nbsp;<?php echo $hesklang['main_page']; ?></a></li>
-          <?php if (hesk_checkPermission('can_man_users',0)){echo '<li><a href="manage_users.php"><i class="fa fa-users" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_users'].'</a></li>';}
-                if (hesk_checkPermission('can_man_cat',0))  {echo '<li><a href="manage_categories.php"><i class="fa fa-pie-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_cat'].'</a></li>';}
-                if (hesk_checkPermission('can_man_canned',0)) {echo '<li><a href="manage_canned.php"><i class="fa fa-file-text-o" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_can'].'</a></li>';}
-                if (hesk_checkPermission('can_man_kb',0)) {echo '<li><a href="manage_knowledgebase.php"><i class="fa fa-book" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_kb'].'</a></li>';}
-                    else {echo '<li><a href="knowledgebase_private.php"><i class="fa fa-book" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_kb'].'</a></li>';}
-                if (hesk_checkPermission('can_run_reports',0)) {echo '<li><a href="reports.php"><i class="fa fa-line-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['reports'].'</a></li>';}
-                    elseif (hesk_checkPermission('can_export',0)) {echo '<li><a href="export.php"><i class="fa fa-line-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['reports'].'</a></li>';}
-                if (hesk_checkPermission('can_manage_settings',0)) {echo '<li><a href="admin_settings.php"><i class="fa fa-cog" '.$iconDisplay.'></i>&nbsp;'.$hesklang['settings'].'</a></li>';} ?>
+          <?php if (hesk_checkPermission('can_man_users',0)) {
+                    echo '<li><a href="manage_users.php"><i class="fa fa-users" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_users'].'</a></li>';
+                }
+                if (hesk_checkPermission('can_man_cat',0))  {
+                    echo '<li><a href="manage_categories.php"><i class="fa fa-pie-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_cat'].'</a></li>';
+                }
+                if (hesk_checkPermission('can_man_canned',0)) {
+                    echo '<li><a href="manage_canned.php"><i class="fa fa-file-text-o" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_can'].'</a></li>';
+                } elseif (hesk_checkPermission('can_man_ticket_tpl',0)) {
+                    echo '<li><a href="manage_ticket_templates.php"><i class="fa fa-file-text-o" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_can'].'</a></li>';
+                }
+                if (hesk_checkPermission('can_man_kb',0)) {
+                    echo '<li><a href="manage_knowledgebase.php"><i class="fa fa-book" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_kb'].'</a></li>';
+                } else {
+                    echo '<li><a href="knowledgebase_private.php"><i class="fa fa-book" '.$iconDisplay.'></i>&nbsp;'.$hesklang['menu_kb'].'</a></li>';
+                }
+                if (hesk_checkPermission('can_run_reports',0)) {
+                    echo '<li><a href="reports.php"><i class="fa fa-line-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['reports'].'</a></li>';
+                } elseif (hesk_checkPermission('can_export',0)) {
+                    echo '<li><a href="export.php"><i class="fa fa-line-chart" '.$iconDisplay.'></i>&nbsp;'.$hesklang['reports'].'</a></li>';
+                }
+                if (hesk_checkPermission('can_ban_emails',0)) {
+                    echo '<li><a href="banned_emails.php"><i class="fa fa-wrench" '.$iconDisplay.'></i>&nbsp;'.$hesklang['tools'].'</a> </li>';
+                } elseif (hesk_checkPermission('can_ban_ips',0)) {
+                    echo '<li><a href="banned_ips.php"><i class="fa fa-wrench" '.$iconDisplay.'></i>&nbsp;'.$hesklang['tools'].'</a> </li>';
+                } elseif (hesk_checkPermission('can_service_msg',0)) {
+                    echo '<li><a href="service_messages.php"><i class="fa fa-wrench" '.$iconDisplay.'></i>&nbsp;'.$hesklang['tools'].'</a> </li>';
+                }
+                if (hesk_checkPermission('can_manage_settings',0)) {
+                    echo '<li><a href="admin_settings.php"><i class="fa fa-cog" '.$iconDisplay.'></i>&nbsp;'.$hesklang['settings'].'</a></li>';
+                }
+          ?>
           <li><a href="profile.php"><i class="fa fa-user" <?php echo $iconDisplay; ?>></i>&nbsp;<?php echo $hesklang['menu_profile']; ?></a></li>
           <li><a href="mail.php"><i class="fa fa-envelope-o" <?php echo $iconDisplay; ?>></i>&nbsp;<?php echo $hesklang['menu_msg']; ?>
               <?php if ($num_mail != 0)
@@ -77,3 +101,19 @@ $num_mail = hesk_checkNewMail();
         </ul>
       </div><!-- /.navbar-collapse -->
     </nav>
+
+    <?php
+    // Show a notice if we are in maintenance mode
+    if ( hesk_check_maintenance(false) )
+    {
+        echo '<br />';
+        hesk_show_notice($hesklang['mma2'], $hesklang['mma1'], false);
+    }
+
+    // Show a notice if we are in "Knowledgebase only" mode
+    if ( hesk_check_kb_only(false) )
+    {
+        echo '<br />';
+        hesk_show_notice($hesklang['kbo2'], $hesklang['kbo1'], false);
+    }
+    ?>
