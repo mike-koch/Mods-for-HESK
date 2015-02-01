c8c33d55d1e65914f43aa42c9286a8f70d9d9cfc
Mike Koch <mkoch227@gmail.com>
2015-02-01 11:39:51 -0500

    Restyle the updateModsForHesk page a bit

--- install/updateModsForHesk.php
+++ install/updateModsForHesk.php
@@ -6,27 +6,156 @@ require(HESK_PATH . 'hesk_settings.inc.php');
 ?>
 <html>
     <head>
-        <title>Mods For HESK 1.7.0 Install / Upgrade</title>
+        <title>Mods For HESK 2.0.0 Install / Upgrade</title>
+        <link href="../hesk_style.css?<?php echo HESK_NEW_VERSION; ?>" type="text/css" rel="stylesheet" />
+        <link href="<?php echo HESK_PATH; ?>css/bootstrap.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
+        <link href="<?php echo HESK_PATH; ?>css/bootstrap-theme.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
+        <link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href="../css/hesk_newStyle.php" type="text/css" rel="stylesheet" />
     </head>
     <body>
-        <h1>Mods for HESK 1.7.0 Install / Upgrade</h1>
-        <h3>Select your current Mods for HESK version number to upgrade.</h3>
-        <ul style="list-style-type: none">
-            <li><a href="updateTo1-7-0.php">v1.6.1</a></li>
-            <li><a href="updateTo1-6-1.php">v1.6.0</a></li>
-            <li><a href="updateTo1-6-0.php">v1.5.0</a></li>
-            <li><a href="updateTo1-5-0.php">v1.4.1</a></li>
-            <li><a href="updateTo1-4-1.php?ar=true">v1.4.0</a></li>
-            <li><a href="updateTo1-4-1.php">v1.2.4 or v1.3.0</a></li>
-            <li><a href="freshInstall.php">No previous version</a> (make sure you have installed/updated HESK first; otherwise installation will <b>fail</b>!)</li>
-        </ul>
-        <p>Please verify the database information below.  Additionally, ensure that the database user has <b>CREATE</b> and <b>ALTER</b> permissions.</p>
-        <p><b>Database Host: </b> <?php echo $hesk_settings['db_host']; ?></p>
-        <p><b>Database Name: </b><?php echo $hesk_settings['db_name']; ?></p>
-        <p><b>Database User: </b><?php echo $hesk_settings['db_user']; ?></p>
-        <p><b>Database Password: </b><?php echo $hesk_settings['db_pass']; ?></p>
-        <p><b>Database Prefix: </b><?php echo $hesk_settings['db_pfix']; ?></p>
-        
-        <p>By proceeding, you agree to the terms of the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License.</a></p>
+        <div class="headersm">Mods for HESK 2.0.0 Install / Upgrade</div>
+        <div class="container">
+            <div class="page-header">
+                <h1>Mods for HESK 2.0.0 Install / Upgrade</h1>
+            </div>
+            <?php
+            $allowInstallation = true;
+            ?>
+            <div class="row">
+                <div class="col-md-5 col-sm-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Database/File Requirements</div>
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th colspan="2">Database Information / File Permissions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>Database Host:</td>
+                                <td><?php echo $hesk_settings['db_host']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Database Name:</td>
+                                <td><?php echo $hesk_settings['db_name']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Database User:</td>
+                                <td><?php echo $hesk_settings['db_user']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Database Password:</td>
+                                <td><?php echo $hesk_settings['db_pass']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>Database Prefix:</td>
+                                <td><?php echo $hesk_settings['db_pfix']; ?></td>
+                            </tr>
+                            <tr>
+                                <td>CREATE, ALTER, DROP Permissions:</td>
+                                <td class="warning"><i class="fa fa-exclamation-triangle"></i> Please check before continuing!*</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    modsForHesk_settings.inc.php
+                                </td>
+                                <?php
+                                $fileperm = substr(sprintf('%o', fileperms(HESK_PATH.'modsForHesk_settings.inc.php')), -4);
+                                $class =  (intval($fileperm) < 666) ? 'class="danger"' : 'class="success"';
+                                ?>
+                                <td <?php echo $class; ?>>
+                                    <?php if ($class == 'class="success"') {
+                                        echo '<i class="fa fa-check-circle"></i> Success';
+                                    } else {
+                                        echo '<i class="fa fa-times-circle"></i> CHMOD to 0666, yours is '.$fileperm;
+                                        $allowInstallation = false;
+                                    }
+                                    ?>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td colspan="2">
+                                    * Mods for HESK is unable to check database permissions automatically.
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-7 col-sm-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Install / Upgrade</div>
+                        <div class="panel-body">
+                            <?php if ($allowInstallation) {
+                                $prereqDiv = 'none';
+                                $installDiv = 'block';
+                            } else {
+                                $prereqDiv = 'block';
+                                $installDiv = 'none';
+                            }
+                            ?>
+                            <div class="prereqsFailedDiv" style="display:<?php echo $prereqDiv; ?>">
+                                <div class="alert alert-danger">
+                                    <p><i class="fa fa-times-circle"></i> You cannot install/upgrade Mods for HESK until the requirements on the left have been met.</p>
+                                    <p><a href="updateModsForHesk.php" class="btn btn-default">Refresh</a></p>
+                                </div>
+                            </div>
+                            <div class="installDiv"  style="display:<?php echo $installDiv; ?>">
+                                <div class="alert alert-info">
+                                    <p><i class="fa fa-exclamation-triangle"></i> Make sure that you have updated / installed HESK first; otherwise installation will <b>fail</b>!</p>
+                                </div>
+                                <p>What version of Mods for HESK do you currently have installed?</p>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo2-0-0.php">v1.7.0</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-7-0.php">v1.6.1</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-6-1.php">v1.6.0</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-6-0.php">v1.5.0</a>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-5-0.php">v1.4.1</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-4-1.php?ar=true">v1.4.0</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-4-1.php">v1.3.0</a>
+                                    </div>
+                                    <div class="col-md-3 col-sm-12">
+                                        <a class="btn btn-default btn-block" href="updateTo1-4-1.php">v1.2.4</a>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <a class="btn btn-default btn-block" href="freshInstall.php">I do not currently have Mods for HESK installed</a>
+                                    </div>
+                                </div>
+                                <br>
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        By proceeding, you agree to the terms of the <a href="https://creativecommons.org/licenses/by-sa/4.0/" target="_blank">Creative Commons Attribution-ShareAlike 4.0 International License.</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <p></p>
+        </div>
     </body>
 </html>
