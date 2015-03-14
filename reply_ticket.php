23dc02399f6567285ce2e854a68fe2f5b95db318
Mike Koch <mkoch227@gmail.com>
2015-03-14 12:53:40 -0400

    Update headers that weren't changed to 2.6.1

--- reply_ticket.php
+++ reply_ticket.php
@@ -1,7 +1,7 @@
 <?php
 /*******************************************************************************
 *  Title: Help Desk Software HESK
-*  Version: 2.6.0 from 22nd February 2015
+*  Version: 2.6.1 from 26th February 2015
 *  Author: Klemen Stirn
 *  Website: http://www.hesk.com
 ********************************************************************************
