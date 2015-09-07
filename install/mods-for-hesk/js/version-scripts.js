6cd71605c116de46d0c78407e11fb7e3d6a8431e
Mike Koch <mkoch227@gmail.com>
2015-09-07 14:01:35 -0400

    fix css classes

--- install/mods-for-hesk/js/version-scripts.js
+++ install/mods-for-hesk/js/version-scripts.js
@@ -1,61 +1,61 @@
 function processUpdates(startingVersion) {
     if (startingVersion < 2) {
         startVersionUpgrade('p140');
-        executeUpdate(2, '2', 'Pre 1.4.0');
+        executeUpdate(2, 'p140', 'Pre 1.4.0');
     } else if (startingVersion < 3) {
         startVersionUpgrade('140');
-        executeUpdate(3, '3', '1.4.0');
+        executeUpdate(3, '140', '1.4.0');
     } else if (startingVersion < 4) {
         startVersionUpgrade('141');
-        executeUpdate(4, '4', '1.4.1');
+        executeUpdate(4, '141', '1.4.1');
     } else if (startingVersion < 5) {
         startVersionUpgrade('150');
-        executeUpdate(5, '5', '1.5.0');
+        executeUpdate(5, '150', '1.5.0');
     } else if (startingVersion < 6) {
         startVersionUpgrade('160');
-        executeUpdate(6, '6', '1.6.0');
+        executeUpdate(6, '160', '1.6.0');
     } else if (startingVersion < 7) {
         startVersionUpgrade('161');
-        executeUpdate(7, '7', '1.6.1');
+        executeUpdate(7, '161', '1.6.1');
     } else if (startingVersion < 8) {
         startVersionUpgrade('170');
-        executeUpdate(8, '8', '1.7.0');
+        executeUpdate(8, '170', '1.7.0');
     } else if (startingVersion < 9) {
         startVersionUpgrade('200');
-        executeUpdate(9, '9', '2.0.0');
+        executeUpdate(9, '200', '2.0.0');
     } else if (startingVersion < 10) {
         startVersionUpgrade('201');
-        executeUpdate(10, '10', '2.0.1');
+        executeUpdate(10, '201', '2.0.1');
     } else if (startingVersion < 11) {
         startVersionUpgrade('210');
-        executeUpdate(11, '11', '2.1.0');
+        executeUpdate(11, '210', '2.1.0');
     } else if (startingVersion < 12) {
         startVersionUpgrade('211');
-        executeUpdate(12, '12', '2.1.1');
+        executeUpdate(12, '211', '2.1.1');
     } else if (startingVersion < 13) {
         startVersionUpgrade('220');
-        executeUpdate(13, '13', '2.2.0');
+        executeUpdate(13, '220', '2.2.0');
     } else if (startingVersion < 14) {
         startVersionUpgrade('221');
-        executeUpdate(14, '14', '2.2.1');
+        executeUpdate(14, '221', '2.2.1');
     } else if (startingVersion < 15) {
         startVersionUpgrade('230');
-        executeUpdate(15, '15', '2.3.0');
+        executeUpdate(15, '230', '2.3.0');
     } else if (startingVersion < 16) {
         startVersionUpgrade('231');
-        executeUpdate(16, '16', '2.3.1');
+        executeUpdate(16, '231', '2.3.1');
     } else if (startingVersion < 17) {
         startVersionUpgrade('232');
-        executeUpdate(17, '17', '2.3.2');
+        executeUpdate(17, '232', '2.3.2');
     } else if (startingVersion < 18) {
         startVersionUpgrade('240');
-        executeUpdate(18, '18', '2.4.0');
+        executeUpdate(18, '240', '2.4.0');
     } else if (startingVersion < 19) {
         startVersionUpgrade('241');
-        executeUpdate(19, '19', '2.4.1');
+        executeUpdate(19, '241', '2.4.1');
     } else if (startingVersion < 20) {
         startVersionUpgrade('242');
-        executeUpdate(20, '20', '2.4.2');
+        executeUpdate(20, '242', '2.4.2');
     } else {
         installationFinished();
     }
