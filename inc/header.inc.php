89fed16339083e8c879091cea45406751c2bdd44
Mike Koch <mkoch227@gmail.com>
2015-09-12 00:25:04 -0400

    Moved common CSS into its own stylesheet

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -57,8 +57,10 @@ $modsForHesk_settings = mfh_getSettings();
 	<link href="<?php echo HESK_PATH; ?>css/bootstrap-theme.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" <?php if ($modsForHesk_settings['use_bootstrap_theme'] == 0) {echo 'disabled';} ?>>
     <?php if ($modsForHesk_settings['rtl']) { ?>
     <link href="<?php echo HESK_PATH; ?>css/bootstrap-rtl.min.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
+    <link href="<?php echo HESK_PATH; ?>css/mods-for-hesk.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
 	<link href="<?php echo HESK_PATH; ?>css/hesk_newStyleRTL.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
     <?php } else { ?>
+    <link href="<?php echo HESK_PATH; ?>css/mods-for-hesk.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
     <link href="<?php echo HESK_PATH; ?>css/hesk_newStyle.css?v=<?php echo $hesk_settings['hesk_version']; ?>" type="text/css" rel="stylesheet" />
     <?php } ?>
     <link href="<?php echo HESK_PATH; ?>css/bootstrap-iconpicker.min.css" rel="stylesheet">
