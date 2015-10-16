8a432ec0bd835a71cde975ed30245246da721c8d
Mike Koch <mkoch227@gmail.com>
2015-10-15 22:31:24 -0400

    #365 Get started on api

---
+++ api/canned/index.php
@@ -0,0 +1,19 @@
+<?php
+define('IN_SCRIPT', 1);
+define('HESK_PATH', '../../');
+require(HESK_PATH . 'hesk_settings.inc.php');
+require(HESK_PATH . 'inc/common.inc.php');
+
+hesk_load_api_database_functions();
+hesk_dbConnect();
+
+// Routing
+if (isset($_GET['id'])) {
+    $response = hesk_dbQuery("SELECT `id`, `message`, `html`, `title`, `reply_order` FROM `"
+        . hesk_dbEscape($hesk_settings['db_pfix']) . "std_replies` WHERE `id` = ".intval($_GET['id']));
+    $result = hesk_dbFetchAssoc($response);
+
+    header('Content-Type: application/json');
+    print json_encode($result);
+    return http_response_code(200);
+}
