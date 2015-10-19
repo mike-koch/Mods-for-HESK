a42ae59d2206f89a6fcb3ff5f3e57bc6fe698d50
Mike Koch <mkoch227@gmail.com>
2015-10-19 00:15:55 -0400

    Always push new settings to table

--- install/mods-for-hesk/sql/installSql.php
+++ install/mods-for-hesk/sql/installSql.php
@@ -556,100 +556,101 @@ function migrateSettings()
 
     hesk_dbConnect();
 
+    $modsForHesk_settings = array();
     if (file_exists(HESK_PATH . 'modsForHesk_settings.inc.php')) {
         require_once(HESK_PATH . 'modsForHesk_settings.inc.php');
-
-        $rtl = getSettingValue($modsForHesk_settings, 'rtl', 0);
-        $show_icons = getSettingValue($modsForHesk_settings, 'show_icons', 0);
-        $custom_field_setting = getSettingValue($modsForHesk_settings, 'custom_field_setting', 0);
-        $customer_email_verification_required = getSettingValue($modsForHesk_settings, 'customer_email_verification_required', 0);
-        $html_emails = getSettingValue($modsForHesk_settings, 'html_emails', 1);
-        $use_mailgun = getSettingValue($modsForHesk_settings, 'use_mailgun', 0);
-        $mailgun_api_key = getSettingValue($modsForHesk_settings, 'mailgun_api_key', '');
-        $mailgun_domain = getSettingValue($modsForHesk_settings, 'mailgun_domain', '');
-        $use_bootstrap_theme = getSettingValue($modsForHesk_settings, 'use_bootstrap_theme', 1);
-        $new_kb_article_visibility = getSettingValue($modsForHesk_settings, 'new_kb_article_visibility', 0);
-        $attachments = getSettingValue($modsForHesk_settings, 'attachments', 0);
-        $show_number_merged = getSettingValue($modsForHesk_settings, 'show_number_merged', 1);
-        $request_location = getSettingValue($modsForHesk_settings, 'request_location', 0);
-        $category_order_column = getSettingValue($modsForHesk_settings, 'category_order_column', 'cat_order');
-        $rich_text_for_tickets = getSettingValue($modsForHesk_settings, 'rich_text_for_tickets', 0);
-        $statuses_order_column = getSettingValue($modsForHesk_settings, 'statuses_order_column', 'sort');
-        $kb_attach_dir = getSettingValue($modsForHesk_settings, 'kb_attach_dir', 'attachments');
-        $rich_text_for_tickets_for_customers = getSettingValue($modsForHesk_settings, 'rich_text_for_tickets_for_customers', 0);
-
-        $navbar_background_color = getSettingValue($modsForHesk_settings, 'navbarBackgroundColor', '#414a5c');
-        $navbar_brand_color = getSettingValue($modsForHesk_settings, 'navbarBrandColor', '#d4dee7');
-        $navbar_brand_hover_color = getSettingValue($modsForHesk_settings, 'navbarBrandHoverColor', '#ffffff');
-        $navbar_item_text_color = getSettingValue($modsForHesk_settings, 'navbarItemTextColor', '#d4dee7');
-        $navbar_item_text_hover_color = getSettingValue($modsForHesk_settings, 'navbarItemTextHoverColor', '#ffffff');
-        $navbar_item_text_selected_color = getSettingValue($modsForHesk_settings, 'navbarItemTextSelectedColor', '#ffffff');
-        $navbar_item_selected_background_color = getSettingValue($modsForHesk_settings, 'navbarItemSelectedBackgroundColor', '#2d3646');
-        $dropdown_item_text_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextColor', '#333333');
-        $dropdown_item_text_hover_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextHoverColor', '#262626');
-        $dropdown_item_text_hover_background_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextHoverBackgroundColor', '#f5f5f5');
-        $question_mark_color = getSettingValue($modsForHesk_settings, 'questionMarkColor', '#000000');
-
-
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('rtl', " . intval($rtl) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('show_icons', " . intval($show_icons) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('custom_field_setting', " . intval($custom_field_setting) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('customer_email_verification_required', " . intval($customer_email_verification_required) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('html_emails', " . intval($html_emails) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('use_mailgun', " . intval($use_mailgun) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('mailgun_api_key', '" . hesk_dbEscape($mailgun_api_key) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('mailgun_domain', '" . hesk_dbEscape($mailgun_domain) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('use_bootstrap_theme', " . intval($use_bootstrap_theme) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('new_kb_article_visibility', " . intval($new_kb_article_visibility) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('attachments', " . intval($attachments) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('show_number_merged', " . intval($show_number_merged) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('request_location', " . intval($request_location) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('category_order_column', '" . hesk_dbEscape($category_order_column) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('rich_text_for_tickets', " . intval($rich_text_for_tickets) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('statuses_order_column', '" . hesk_dbEscape($statuses_order_column) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('kb_attach_dir', '" . hesk_dbEscape($kb_attach_dir) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('rich_text_for_tickets_for_customers', " . intval($rich_text_for_tickets_for_customers) . ")");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarBackgroundColor', '" . hesk_dbEscape($navbar_background_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarBrandColor', '" . hesk_dbEscape($navbar_brand_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarBrandHoverColor', '" . hesk_dbEscape($navbar_brand_hover_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarItemTextColor', '" . hesk_dbEscape($navbar_item_text_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarItemTextHoverColor', '" . hesk_dbEscape($navbar_item_text_hover_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarItemTextSelectedColor', '" . hesk_dbEscape($navbar_item_text_selected_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('navbarItemSelectedBackgroundColor', '" . hesk_dbEscape($navbar_item_selected_background_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('dropdownItemTextColor', '" . hesk_dbEscape($dropdown_item_text_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('dropdownItemTextHoverColor', '" . hesk_dbEscape($dropdown_item_text_hover_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('dropdownItemTextHoverBackgroundColor', '" . hesk_dbEscape($dropdown_item_text_hover_background_color) . "')");
-        executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
-            VALUES ('questionMarkColor', '" . hesk_dbEscape($question_mark_color) . "')");
     }
+
+    $rtl = getSettingValue($modsForHesk_settings, 'rtl', 0);
+    $show_icons = getSettingValue($modsForHesk_settings, 'show_icons', 0);
+    $custom_field_setting = getSettingValue($modsForHesk_settings, 'custom_field_setting', 0);
+    $customer_email_verification_required = getSettingValue($modsForHesk_settings, 'customer_email_verification_required', 0);
+    $html_emails = getSettingValue($modsForHesk_settings, 'html_emails', 1);
+    $use_mailgun = getSettingValue($modsForHesk_settings, 'use_mailgun', 0);
+    $mailgun_api_key = getSettingValue($modsForHesk_settings, 'mailgun_api_key', '');
+    $mailgun_domain = getSettingValue($modsForHesk_settings, 'mailgun_domain', '');
+    $use_bootstrap_theme = getSettingValue($modsForHesk_settings, 'use_bootstrap_theme', 1);
+    $new_kb_article_visibility = getSettingValue($modsForHesk_settings, 'new_kb_article_visibility', 0);
+    $attachments = getSettingValue($modsForHesk_settings, 'attachments', 0);
+    $show_number_merged = getSettingValue($modsForHesk_settings, 'show_number_merged', 1);
+    $request_location = getSettingValue($modsForHesk_settings, 'request_location', 0);
+    $category_order_column = getSettingValue($modsForHesk_settings, 'category_order_column', 'cat_order');
+    $rich_text_for_tickets = getSettingValue($modsForHesk_settings, 'rich_text_for_tickets', 0);
+    $statuses_order_column = getSettingValue($modsForHesk_settings, 'statuses_order_column', 'sort');
+    $kb_attach_dir = getSettingValue($modsForHesk_settings, 'kb_attach_dir', 'attachments');
+    $rich_text_for_tickets_for_customers = getSettingValue($modsForHesk_settings, 'rich_text_for_tickets_for_customers', 0);
+
+    $navbar_background_color = getSettingValue($modsForHesk_settings, 'navbarBackgroundColor', '#414a5c');
+    $navbar_brand_color = getSettingValue($modsForHesk_settings, 'navbarBrandColor', '#d4dee7');
+    $navbar_brand_hover_color = getSettingValue($modsForHesk_settings, 'navbarBrandHoverColor', '#ffffff');
+    $navbar_item_text_color = getSettingValue($modsForHesk_settings, 'navbarItemTextColor', '#d4dee7');
+    $navbar_item_text_hover_color = getSettingValue($modsForHesk_settings, 'navbarItemTextHoverColor', '#ffffff');
+    $navbar_item_text_selected_color = getSettingValue($modsForHesk_settings, 'navbarItemTextSelectedColor', '#ffffff');
+    $navbar_item_selected_background_color = getSettingValue($modsForHesk_settings, 'navbarItemSelectedBackgroundColor', '#2d3646');
+    $dropdown_item_text_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextColor', '#333333');
+    $dropdown_item_text_hover_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextHoverColor', '#262626');
+    $dropdown_item_text_hover_background_color = getSettingValue($modsForHesk_settings, 'dropdownItemTextHoverBackgroundColor', '#f5f5f5');
+    $question_mark_color = getSettingValue($modsForHesk_settings, 'questionMarkColor', '#000000');
+
+
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('rtl', " . intval($rtl) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('show_icons', " . intval($show_icons) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('custom_field_setting', " . intval($custom_field_setting) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('customer_email_verification_required', " . intval($customer_email_verification_required) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('html_emails', " . intval($html_emails) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('use_mailgun', " . intval($use_mailgun) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('mailgun_api_key', '" . hesk_dbEscape($mailgun_api_key) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('mailgun_domain', '" . hesk_dbEscape($mailgun_domain) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('use_bootstrap_theme', " . intval($use_bootstrap_theme) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('new_kb_article_visibility', " . intval($new_kb_article_visibility) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('attachments', " . intval($attachments) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('show_number_merged', " . intval($show_number_merged) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('request_location', " . intval($request_location) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('category_order_column', '" . hesk_dbEscape($category_order_column) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('rich_text_for_tickets', " . intval($rich_text_for_tickets) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('statuses_order_column', '" . hesk_dbEscape($statuses_order_column) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('kb_attach_dir', '" . hesk_dbEscape($kb_attach_dir) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('rich_text_for_tickets_for_customers', " . intval($rich_text_for_tickets_for_customers) . ")");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarBackgroundColor', '" . hesk_dbEscape($navbar_background_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarBrandColor', '" . hesk_dbEscape($navbar_brand_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarBrandHoverColor', '" . hesk_dbEscape($navbar_brand_hover_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarItemTextColor', '" . hesk_dbEscape($navbar_item_text_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarItemTextHoverColor', '" . hesk_dbEscape($navbar_item_text_hover_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarItemTextSelectedColor', '" . hesk_dbEscape($navbar_item_text_selected_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('navbarItemSelectedBackgroundColor', '" . hesk_dbEscape($navbar_item_selected_background_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('dropdownItemTextColor', '" . hesk_dbEscape($dropdown_item_text_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('dropdownItemTextHoverColor', '" . hesk_dbEscape($dropdown_item_text_hover_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('dropdownItemTextHoverBackgroundColor', '" . hesk_dbEscape($dropdown_item_text_hover_background_color) . "')");
+    executeQuery("INSERT INTO `" . hesk_dbEscape($hesk_settings['db_pfix']) . "settings` (`Key`, `Value`)
+        VALUES ('questionMarkColor', '" . hesk_dbEscape($question_mark_color) . "')");
 }
 
 function getSettingValue($settings, $setting, $default)
