8985413901db0cd1fe51521a8b3ced5a8d62468f
Mike Koch <mkoch227@gmail.com>
2015-11-01 11:58:30 -0500

    #407 Add endpoint for canned responses

--- api/canned/index.php
+++ api/canned/index.php
@@ -9,9 +9,10 @@ hesk_dbConnect();
 
 // Routing
 if (isset($_GET['id'])) {
-    $response = hesk_dbQuery("SELECT `id`, `message`, `html`, `title`, `reply_order` FROM `"
+    $response = hesk_dbQuery("SELECT `id`, `message`, `title`, `reply_order` FROM `"
         . hesk_dbEscape($hesk_settings['db_pfix']) . "std_replies` WHERE `id` = ".intval($_GET['id']));
     $result = hesk_dbFetchAssoc($response);
+    $result['message'] = html_entity_decode($result['message']);
 
     header('Content-Type: application/json');
     print json_encode($result);
