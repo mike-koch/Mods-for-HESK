93f98484d9ea2f3b266fa625537b7c880e5c5124
Mike Koch <mkoch227@gmail.com>
2015-11-05 21:55:23 -0500

    #412 Add /admin/category

--- api/dao/category_dao.php
+++ api/dao/category_dao.php
@@ -1,5 +1,27 @@
 <?php
 
 function get_category($hesk_settings, $id = NULL) {
-    return NULL;
+    $sql = "SELECT * FROM `" . hesk_dbEscape($hesk_settings['db_pfix']) . "categories` ";
+    if ($id != NULL) {
+        $sql .= "WHERE `id` = ".intval($id);
+    }
+
+    $response = hesk_dbQuery($sql);
+
+    if (hesk_dbNumRows($response) == 0) {
+        return NULL;
+    }
+
+    $results = [];
+    while ($row = hesk_dbFetchAssoc($response)) {
+        $row['id'] = intval($row['id']);
+        $row['cat_order'] = intval($row['cat_order']);
+        $row['autoassign'] = intval($row['autoassign']);
+        $row['type'] = intval($row['type']);
+        $row['priority'] = intval($row['priority']);
+        $row['manager'] = intval($row['manager']);
+        $results[] = $row;
+    }
+
+    return $id == NULL ? $results : $results[0];
 }
