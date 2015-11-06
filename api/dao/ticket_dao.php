ff4c3bfc99e941323ae1d1b7a9495999e17d61fe
Mike Koch <mkoch227@gmail.com>
2015-11-05 22:01:14 -0500

    Improve ticket API

--- api/dao/ticket_dao.php
+++ api/dao/ticket_dao.php
@@ -14,6 +14,23 @@ function get_ticket_for_id($hesk_settings, $id = NULL) {
 
     $results = [];
     while ($row = hesk_dbFetchAssoc($response)) {
+        $row['id'] = intval($row['id']);
+        $row['category'] = intval($row['category']);
+        $row['priority'] = intval($row['priority']);
+        $row['status'] = intval($row['status']);
+        $row['openedby'] = intval($row['openedby']);
+        $row['lastreplier'] = intval($row['lastreplier']);
+        $row['replierid'] = intval($row['replierid']);
+        $row['archive'] = $row['archive'] == true;
+        $row['locked'] = $row['locked'] == true;
+        $row['html'] = $row['html'] == true;
+        $row['screen_resolution_height'] = $row['screen_resolution_height'] != NULL
+            ? intval($row['screen_resolution_height'])
+            : NULL;
+        $row['screen_resolution_width'] = $row['screen_resolution_width'] != NULL
+            ? intval($row['screen_resolution_width'])
+            : NULL;
+
         $results[] = $row;
     }
 
