1bdd658a7d670a4c10b18e3317ae9545020e7174
Mike Koch <mkoch227@gmail.com>
2015-12-27 14:57:51 -0500

    Attachments can be removed via AJAX

--- inc/header.inc.php
+++ inc/header.inc.php
@@ -115,6 +115,7 @@ if (is_dir(HESK_PATH . 'install')) {
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
     <script type="text/javascript" src="<?php echo HESK_PATH; ?>js/platform.js"></script>
     <script type="text/javascript" src="<?php echo HESK_PATH; ?>js/bootstrap-validator.min.js"></script>
+    <script type="text/javascript" src="<?php echo HESK_PATH; ?>internal-api/js/core.php"></script>
     <style>
         .navbar-default {
             background-color: <?php echo $modsForHesk_settings['navbarBackgroundColor']; ?>;
