5e21877efc8807ee25a92043fb859ca5d4c64c0b
Mike Koch <mkoch227@gmail.com>
2017-01-17 22:12:45 -0500

    Add Link for simple API routing

--- api/index.php
+++ api/index.php
@@ -1,8 +1,14 @@
 <?php
+//define('IN_SCRIPT', 1);
+//define('HESK_PATH', '../');
 // Router: handles all REST requests to go to their proper place. Common dependency loading also happens here
-require_once(__DIR__ . '/core/common.php');
+//require_once(__DIR__ . '/core/common.php');
+require(__DIR__ . '/../Link/src/Link.php');
 
-// Returns the URI (or everything after the domain
-$uri = $_SERVER["REQUEST_URI"];
+function routeMe(){
+    echo 'I am routed';
+}
 
-// 
+Link::all( array(
+    '/test' => 'routeMe',
+));
