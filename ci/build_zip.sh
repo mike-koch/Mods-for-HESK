a2b44cd288e54fec3e211bf3ad6971f5f8298fa6
Mike Koch <mkoch227@gmail.com>
2017-06-25 21:13:31 -0400

    Change the name of the zip

--- ci/build_zip.sh
+++ ci/build_zip.sh
@@ -7,12 +7,10 @@ TARGETDIR=$VERSION_NUMBER;for file in *;do test "$file" != "$TARGETDIR" && cp -r
 # Remove files that we don't want to bundle
 cd $VERSION_NUMBER
 rm -rf ci
-#rm .gitignore
-#rm .gitlab-ci.yml
 rm -rf .git
 rm apidoc.json
 rm CONTRIBUTING.md
 
 cd ../
-zip -r release.zip $VERSION_NUMBER
+zip -r $VERSION_NUMBER.zip $VERSION_NUMBER
 rm -rf $VERSION_NUMBER
