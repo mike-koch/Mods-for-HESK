73b3a01bc0470bf38ef80a552ca13d8f8e202bc6
Mike Koch <mkoch227@gmail.com>
2017-08-10 21:51:59 -0400

    Version checks are now asynchronous

--- api/index.php
+++ api/index.php
@@ -15,24 +15,35 @@ function handle404() {
     ), 404);
 }
 
-function before() {
-    if (defined('HESK_DEMO') && $_SERVER['REQUEST_METHOD'] !== 'GET') {
+function globalBefore() {
+    if (defined('HESK_DEMO') && $_SERVER['REQUEST_METHOD'] === 'GET') {
         print_error('Demo Mode', 'Only read-only commands are available in demo mode!', null, 401);
         die();
     }
+}
 
+function internalHandler() {
+    buildUserContextFromSession();
+}
+
+function authTokenHandler() {
+    assertApiIsEnabled();
+    $token = \BusinessLogic\Helpers::getHeader('X-AUTH-TOKEN');
+    buildUserContext($token);
+}
+
+function internalOrAuthHandler() {
     $internalUse = \BusinessLogic\Helpers::getHeader('X-INTERNAL-CALL');
 
     if ($internalUse === 'true') {
-        buildUserContextFromSession();
-    } elseif (preg_match('/\/v1\/tickets\/.+\/attachments\/\d+/', $_SERVER['PATH_INFO'])) {
-        //-- TODO Clean this up
-        return;
+        internalHandler();
     } else {
-        assertApiIsEnabled();
-        $token = \BusinessLogic\Helpers::getHeader('X-AUTH-TOKEN');
-        buildUserContext($token);
+        authTokenHandler();
     }
+}
+
+function publicHandler() {
+    //-- No-op
 }
 
 function assertApiIsEnabled() {
@@ -172,34 +183,55 @@ function fatalErrorShutdownHandler() {
     }
 }
 
-Link::before('before');
+Link::before('globalBefore');
 
 Link::all(array(
     // Categories
-    '/v1/categories' => [\Controllers\Categories\CategoryController::class . '::printAllCategories'],
-    '/v1/categories/{i}' => \Controllers\Categories\CategoryController::class,
+    '/v1/categories' => action(\Controllers\Categories\CategoryController::class . '::printAllCategories'),
+    '/v1/categories/{i}' => action(\Controllers\Categories\CategoryController::class),
     // Tickets
-    '/v1/tickets' => \Controllers\Tickets\CustomerTicketController::class,
+    '/v1/tickets' => action(\Controllers\Tickets\CustomerTicketController::class),
     // Tickets - Staff
-    '/v1/staff/tickets/{i}' => \Controllers\Tickets\StaffTicketController::class,
+    '/v1/staff/tickets/{i}' => action(\Controllers\Tickets\StaffTicketController::class),
     // Attachments
-    '/v1/tickets/{a}/attachments/{i}' => \Controllers\Attachments\PublicAttachmentController::class . '::getRaw',
-    '/v1/staff/tickets/{i}/attachments' => \Controllers\Attachments\StaffTicketAttachmentsController::class,
-    '/v1/staff/tickets/{i}/attachments/{i}' => \Controllers\Attachments\StaffTicketAttachmentsController::class,
+    '/v1/tickets/{a}/attachments/{i}' => action(\Controllers\Attachments\PublicAttachmentController::class . '::getRaw'),
+    '/v1/staff/tickets/{i}/attachments' => action(\Controllers\Attachments\StaffTicketAttachmentsController::class),
+    '/v1/staff/tickets/{i}/attachments/{i}' => action(\Controllers\Attachments\StaffTicketAttachmentsController::class),
     // Statuses
-    '/v1/statuses' => \Controllers\Statuses\StatusController::class,
+    '/v1/statuses' => action(\Controllers\Statuses\StatusController::class),
     // Settings
-    '/v1/settings' => \Controllers\Settings\SettingsController::class,
+    '/v1/settings' => action(\Controllers\Settings\SettingsController::class),
 
     /* Internal use only routes */
     // Resend email response
-    '/v1-internal/staff/tickets/{i}/resend-email' => \Controllers\Tickets\ResendTicketEmailToCustomerController::class,
+    '/v1-internal/staff/tickets/{i}/resend-email' =>
+        action(\Controllers\Tickets\ResendTicketEmailToCustomerController::class, SecurityHandler::INTERNAL),
     // Custom Navigation
-    '/v1-internal/custom-navigation/all' => \Controllers\Navigation\CustomNavElementController::class . '::getAll',
-    '/v1-internal/custom-navigation' => \Controllers\Navigation\CustomNavElementController::class,
-    '/v1-internal/custom-navigation/{i}' => \Controllers\Navigation\CustomNavElementController::class,
-    '/v1-internal/custom-navigation/{i}/sort/{s}' => \Controllers\Navigation\CustomNavElementController::class . '::sort',
+    '/v1-internal/custom-navigation/all' =>
+        action(\Controllers\Navigation\CustomNavElementController::class . '::getAll', SecurityHandler::INTERNAL),
+    '/v1-internal/custom-navigation' =>
+        action(\Controllers\Navigation\CustomNavElementController::class, SecurityHandler::INTERNAL),
+    '/v1-internal/custom-navigation/{i}' =>
+        action(\Controllers\Navigation\CustomNavElementController::class, SecurityHandler::INTERNAL),
+    '/v1-internal/custom-navigation/{i}/sort/{s}' =>
+        action(\Controllers\Navigation\CustomNavElementController::class . '::sort', SecurityHandler::INTERNAL),
+
+    '/v1-public/hesk-version' =>
+        action(\Controllers\System\HeskVersionController::class . '::getHeskVersion', SecurityHandler::OPEN),
+    '/v1-public/mods-for-hesk-version' =>
+        action(\Controllers\System\HeskVersionController::class . '::getModsForHeskVersion', SecurityHandler::OPEN),
 
     // Any URL that doesn't match goes to the 404 handler
     '404' => 'handle404'
 ));
+
+function action($class, $securityHandler = SecurityHandler::AUTH_TOKEN) {
+    return [$class, $class, $securityHandler];
+}
+
+class SecurityHandler {
+    const OPEN = 'publicHandler';
+    const INTERNAL = 'internalHandler';
+    const AUTH_TOKEN = 'authTokenHandler';
+    const INTERNAL_OR_AUTH_TOKEN = 'internalOrAuthHandler';
+}
