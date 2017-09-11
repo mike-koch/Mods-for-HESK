560197eebeeb25af7c34cad12516a217426b26f7
Mike Koch <mkoch227@gmail.com>
2017-09-11 12:34:58 -0400

    Testing some gitlab ci changes

---
+++ ci/php_lint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+error=false
+
+while test $# -gt 0; do
+    current=$1
+    shift
+
+    if [ ! -d $current ] && [ ! -f $current ] ; then
+        echo "Invalid directory or file: $current"
+        error=true
+
+        continue
+    fi
+
+    for file in `find $current -type f -not -path "*vendor/*" -name "*.php"` ; do
+        RESULTS=`php -l $file`
+		echo $RESULTS
+
+        if [ "$RESULTS" != "No syntax errors detected in $file" ] ; then
+            echo $RESULTS
+            error=true
+        fi
+    done
+done
+
+
+if [ "$error" = true ] ; then
+    exit 1
+else
+    exit 0
+fi
+
+
