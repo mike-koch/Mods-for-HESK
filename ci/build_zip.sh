d62370b0696ab533965bc7c64911d820b07b8567
Mike Koch <mkoch227@gmail.com>
2017-11-04 22:17:47 -0400

    Revert "Modify build config"
    This reverts commit 05617a3b56a905ceea16713d6cc5eb489631b325.

---
+++ ci/build_zip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd ../
+mkdir $VERSION_NUMBER
+TARGETDIR=$VERSION_NUMBER;for file in *;do test "$file" != "$TARGETDIR" && cp -r "$file" "$TARGETDIR/";done
+
+# Remove files that we don't want to bundle
+cd $VERSION_NUMBER
+rm -rf ci
+rm -rf .git
+rm apidoc.json
+rm CONTRIBUTING.md
+
+cd ../
+zip -r release.zip $VERSION_NUMBER
+rm -rf $VERSION_NUMBER
