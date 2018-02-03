90dc2d691a39133332fc405fa01be6e8c15ead1c
Mike Koch <mkoch227@gmail.com>
2018-02-02 22:05:23 -0500

    Deactivated some migrations as they're no longer needed.... might break some new installs

--- install/migrations/Pre140/Statuses/AddIntColumnUpDropTableDown.php
+++ install/migrations/Pre140/Statuses/AddIntColumnUpDropTableDown.php
@@ -7,10 +7,12 @@ use AbstractMigration;
 class AddIntColumnUpDropTableDown extends AbstractMigration {
 
     function up($hesk_settings) {
-        $this->executeQuery("ALTER TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` ADD COLUMN `status_int` INT NOT NULL DEFAULT 0 AFTER `status`;");
+        // We no longer need to do this thanks to HESK 2.7.0
+        //$this->executeQuery("ALTER TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` ADD COLUMN `status_int` INT NOT NULL DEFAULT 0 AFTER `status`;");
     }
 
     function down($hesk_settings) {
-        $this->executeQuery("DROP TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "statuses`");
+        // Moved to migration #2 for clarity
+        //$this->executeQuery("DROP TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "statuses`");
     }
 }
--- install/migrations/Pre140/Statuses/DropOldStatusColumn.php
+++ install/migrations/Pre140/Statuses/DropOldStatusColumn.php
@@ -8,14 +8,16 @@ use AbstractMigration;
 class DropOldStatusColumn extends AbstractMigration {
 
     function up($hesk_settings) {
-        $this->executeQuery("ALTER TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` DROP COLUMN `status`");
+        // We no longer need to do this thanks to HESK 2.7.0
+        //$this->executeQuery("ALTER TABLE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` DROP COLUMN `status`");
     }
 
     function down($hesk_settings) {
-        $ticketsRS = $this->executeQuery("SELECT `id`, `status_int` FROM `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets`;");
-        while ($currentResult = hesk_dbFetchAssoc($ticketsRS)) {
-
-            $this->executeQuery("UPDATE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` SET `status_int` = '" . intval($currentResult['status']) . "' WHERE `id` = " . $currentResult['id']);
-        }
+        // We no longer need to do this thanks to HESK 2.7.0
+        //$ticketsRS = $this->executeQuery("SELECT `id`, `status_int` FROM `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets`;");
+        //while ($currentResult = hesk_dbFetchAssoc($ticketsRS)) {
+        //
+        //    $this->executeQuery("UPDATE `" . hesk_dbEscape($hesk_settings['db_pfix']) . "tickets` SET `status_int` = '" . intval($currentResult['status_int']) . "' WHERE `id` = " . $currentResult['id']);
+        //}
     }
 }
